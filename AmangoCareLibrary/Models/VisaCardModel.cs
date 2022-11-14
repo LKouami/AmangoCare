6b3a5cf79e858ade26f9e057f8abac9cb9c54a97
Lucas KOUAMI <lskouami@gmail.com>
2022-11-14 22:36:18 +0000

    still working on crud

--- AmangoCareLibrary/Models/VisaCardModel.cs
+++ AmangoCareLibrary/Models/VisaCardModel.cs
@@ -6,7 +6,7 @@
         [BsonRepresentation(BsonType.ObjectId)]
         public string VisaCardId { get; set; }
         public string VisaCardNumber { get; set; }
-        public DateTime VisaCardExpirationDate { get; set; }
+        public DateTime VisaCardExpirationDate { get; set; } = DateTime.UtcNow;
         public DateTime DateCreated { get; set; } = DateTime.UtcNow;
         public DateTime DateUpdated { get; set; }
         public bool VisaCardState { get; set; }
